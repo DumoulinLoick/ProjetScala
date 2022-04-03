9be3085ec35520a435b32d63e032f8f0b6df2fb5
pailhemath <79859177+pailhemath@users.noreply.github.com>
2022-04-03 20:51:10 +0200

    mise en ligne de reconnexion

--- graph_squelette/src/main/scala/applications/Antenne.scala
+++ graph_squelette/src/main/scala/applications/Antenne.scala
@@ -1,5 +1,7 @@
 package applications
 
+import scala.math.{acos, sin, cos, sqrt, pow, toRadians}
+
 final case class Antenne
 ( 
   id : String, //1
@@ -18,5 +20,13 @@ final case class Antenne
   //cacul de la latitude et longitude
     val latitude = (COR_NB_DG_LAT + COR_NB_MN_LAT/60.0 + COR_CD_NS_LAT/3600.0) * (if ( COR_NB_SC_LAT== "S") 1 else -1)
     val longitude = (COR_NB_DG_LON + COR_NB_MN_LON/60.0 + COR_CD_EW_LON/3600.0) * (if (COR_NB_SC_LON == "W") 1 else -1)
+
+     def distance( latitude1 : Double, longitude1 : Double) : Double = {
+       /* acos( 
+            sin(toRadians(latitude1)) * sin(toRadians(latitude))
+            + cos(toRadians(latitude1)) * cos(toRadians(latitude)) * cos(toRadians(longitude1-longitude))
+        ) * 6371*/
+        852
+    }
 
 }
--- graph_squelette/src/main/scala/applications/Reconnexion.scala
+++ graph_squelette/src/main/scala/applications/Reconnexion.scala
@@ -1,20 +1,48 @@
 package applications
 
 import undirected.{Edge, SimpleGraphDefaultImpl,SimpleGraphMatrixImpl,SimpleGraphNeighborsImpl}
-import scala.collection.immutable.ListMap
+import scala.collection.immutable
+import scala.io.Source
+
+
 
 object Reconnexion extends App {
 
-    val antennes = Reader.getAntennes()//.filter(_.departement == "16166")
-    println(s"${antennes.size} antennes")
+    def transform(antennes : Set[Antennes]) : Map[Edge[V], Double]  = {
+        
 
+
+    }
+
+
+    val antennes = Reader.getAntennes().filter(_.departement == "16166")
+    println(s"${antennes.size} antennes")  
 
     val graph = SimpleGraphDefaultImpl(antennes, Set[Edge[Antenne]]())
-    
+    //val applicationTREE = graph.minimumSpanningTree(valuation)
+    val test = antennes.toList
+    test.foreach(println)   
+     test.foreach(println)   
+    //test.split(" ").foreach(println)(",")//.foreach(println)
+    println(s"aaaa ${test(1)} aaaaa")
+    //tests perso
+    /*val test = antennes.toList
+    test.foreach(println)
+    val a = test.
+    antennes.distance(10.2,11.5)*/
+
+
+
+
     //val coutReconnexion = graph.edges.map(edge => (edge, edge.length)).toMap
     //println(s"${coutReconnexion.size} arcs")
     //caluation : une map associe un arc a une longueur
+
     // valuation Map[Edge[V], Double]
 
-    //val applicationTREE = graph.minimumSpanningTree(valuation)
+    //48.97067508254935, 2.3286418451428674
+
+    //println(antennes.distance(48.97067508254935,2.3286418451428674))
+    //val a = antennes.Distance(48.97067508254935,2.3286418451428674)
+    //
 }
